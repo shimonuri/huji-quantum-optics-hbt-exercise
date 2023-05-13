4c680a08063369dc5527c9bc1a85a88d0a7ab3f9
Uri Shimon <54213090+shimonuri@users.noreply.github.com>
2023-05-13 20:30:06 +0300

    solved several questions

--- sun.py
+++ sun.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import dataclasses
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
 # increase font size
 plt.rcParams.update({"font.size": 22})
@@ -53,12 +54,8 @@ def get_second_coherence_function():
     )
 
 
-def main():
-    plot_sun_fields()
-
-
 def plot_sun_fields():
-    suns = [get_field() for i in range(3)]
+    suns = [get_field() for i in range(2)]
     for i, sun in enumerate(suns):
         plt.plot(sun.intensity, label=f"Sun {i+1}")
 
@@ -67,9 +64,35 @@ def plot_sun_fields():
     plt.legend()
     plt.show()
 
+    ax = plt.axes(projection="3d")
+    # inprove spacing between text and numbers
+    ax.xaxis.labelpad = 25
+    ax.yaxis.labelpad = 25
+    ax.zaxis.labelpad = 25
+    for i, sun in enumerate(suns):
+        # plot  complex vector
+        ax.scatter(
+            range(1024),
+            np.real(sun.vector),
+            np.imag(sun.vector),
+            label=f"Sun {i + 1}",
+        )
+    # plot 3d unit circle
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x = np.outer(np.cos(u), np.sin(v))
+    y = np.outer(np.sin(u), np.sin(v))
+    z = np.outer(np.ones(np.size(u)), np.cos(v))
+    ax.plot_surface(x + 500, y, z, color="black", alpha=0.1)
+    ax.set_xlabel("Cell Number")
+    ax.set_ylabel("Real Part")
+    ax.set_zlabel("Imaginary Part")
+    ax.legend()
+    plt.show()
+
 
 def plot_average_sun_field():
-    suns = [get_field() for i in range(10**5)]
+    suns = [get_field() for _ in range(10000)]
     plt.plot(np.mean([sun.intensity for sun in suns], axis=0))
     plt.xlabel("Cell number")
     plt.ylabel("Intensity")
@@ -78,12 +101,37 @@ def plot_average_sun_field():
 
 def plot_numeric_second_coherence():
     second_coherence = get_second_coherence_function()
-    plt.plot(np.array(range(1024)) - 500, np.abs(second_coherence))
-    plt.axvline(x=-20, color="black", linestyle="--")
-    plt.axvline(x=21, color="black", linestyle="--")
+    x_points = np.array(range(1024)) - 500
+
+    def fit(x, t_c):
+        return 1 + np.exp(-(2 * np.abs(x)) / t_c)
+
+    result = curve_fit(fit, x_points, np.abs(second_coherence))
+
+    plt.plot(x_points, np.abs(second_coherence))
+    plt.plot(
+        x_points,
+        [fit(x, result[0][0]) for x in x_points],
+        label=fr"Fit, $\tau_c$ = {result[0][0]:.2f}",
+    )
+    # plt.axvline(x_points=-20, color="black", linestyle="--")
+    # plt.axvline(x_points=21, color="black", linestyle="--")
     plt.xlabel("Distance from the stationary point (sun center)")
     plt.ylabel("Second order coherence function")
+    plt.legend()
     plt.show()
+
+
+def plot_intensity_histogram():
+    suns = [get_field() for _ in range(10000)]
+    plt.hist([sun.intensity for sun in suns])
+    plt.xlabel("Intensity")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def main():
+    plot_numeric_second_coherence()
 
 
 if __name__ == "__main__":
