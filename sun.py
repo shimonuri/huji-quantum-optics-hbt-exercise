1b786ae339197f7e819a3772c36946b2b7f379fd
Uri Shimon <54213090+shimonuri@users.noreply.github.com>
2023-05-15 08:53:22 +0300

    almost finished exercise

--- sun.py
+++ sun.py
@@ -3,6 +3,7 @@ import random
 import dataclasses
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
+import pandas
 
 # increase font size
 plt.rcParams.update({"font.size": 22})
@@ -24,30 +25,33 @@ class Experiment:
     stationary_intensity: float
 
 
-def initialize_sun_data():
-    sun_data = [0] * 1024
-    for i in range(480, 521):
+def initialize_sun_data(total_size, sun_size, sun_center=500):
+    sun_data = [0] * total_size
+    for i in range(sun_center - sun_size // 2, sun_center + sun_size // 2 + 1):
         sun_data[i] = np.exp(random.random() * 2 * np.pi * 1j)
 
     return np.array(sun_data)
 
 
-def get_field():
-    sun_data = initialize_sun_data()
+def get_field(total_size, sun_size):
+    sun_data = initialize_sun_data(total_size, sun_size)
     fft_sun_data = np.fft.fft(sun_data)
     return Sun(vector=sun_data, field=fft_sun_data)
 
 
-def make_experiment(stationary):
-    field = get_field()
+def make_experiment(stationary, total_size, sun_size):
+    field = get_field(total_size, sun_size)
     return Experiment(
         intensity_product=field.intensity * field.intensity[stationary],
         stationary_intensity=field.intensity[stationary],
     )
 
 
-def get_second_coherence_function():
-    experiments = [make_experiment(500) for i in range(10**5)]
+def get_second_coherence_function(total_size, sun_size):
+    experiments = [
+        make_experiment(500, total_size=total_size, sun_size=sun_size)
+        for _ in range(10**5)
+    ]
     return (
         np.mean([experiment.intensity_product for experiment in experiments], axis=0)
         / np.mean([experiment.stationary_intensity for experiment in experiments]) ** 2
@@ -55,7 +59,7 @@ def get_second_coherence_function():
 
 
 def plot_sun_fields():
-    suns = [get_field() for i in range(2)]
+    suns = [get_field(1024, 40) for i in range(2)]
     for i, sun in enumerate(suns):
         plt.plot(sun.intensity, label=f"Sun {i+1}")
 
@@ -92,7 +96,7 @@ def plot_sun_fields():
 
 
 def plot_average_sun_field():
-    suns = [get_field() for _ in range(10000)]
+    suns = [get_field(1024, 40) for _ in range(10000)]
     plt.plot(np.mean([sun.intensity for sun in suns], axis=0))
     plt.xlabel("Cell number")
     plt.ylabel("Intensity")
@@ -100,7 +104,7 @@ def plot_average_sun_field():
 
 
 def plot_numeric_second_coherence():
-    second_coherence = get_second_coherence_function()
+    second_coherence = get_second_coherence_function(total_size=1024, sun_size=40)
     x_points = np.array(range(1024)) - 500
 
     def fit(x, t_c):
@@ -112,7 +116,7 @@ def plot_numeric_second_coherence():
     plt.plot(
         x_points,
         [fit(x, result[0][0]) for x in x_points],
-        label=fr"Fit, $\tau_c$ = {result[0][0]:.2f}",
+        label=rf"Fit, $\tau_c$ = {result[0][0]:.2f}",
     )
     # plt.axvline(x_points=-20, color="black", linestyle="--")
     # plt.axvline(x_points=21, color="black", linestyle="--")
@@ -123,10 +127,33 @@ def plot_numeric_second_coherence():
 
 
 def plot_intensity_histogram():
-    suns = [get_field() for _ in range(10000)]
+    suns = [get_field(1024, 40) for _ in range(10000)]
     plt.hist([sun.intensity for sun in suns])
     plt.xlabel("Intensity")
     plt.ylabel("Frequency")
+    plt.show()
+
+
+def find_fwhm():
+    for sun_size in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
+        second_coherence = get_second_coherence_function(
+            total_size=1024, sun_size=sun_size
+        )
+        x_points = np.array(range(1024)) - 500
+        plt.plot(
+            x_points,
+            second_coherence,
+        )
+        plt.axhline(y=0.5 * np.max(abs(second_coherence)))
+        plt.title(f"Sun size = {sun_size}")
+        plt.show()
+
+
+def plot_fwhm_from_file():
+    csv = pandas.read_csv("fwhm.csv")
+    plt.plot(csv["size"], csv["FWHM"])
+    plt.xlabel("Sun size")
+    plt.ylabel("FWHM")
     plt.show()
 
 
